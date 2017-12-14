a661fff53e220f6b93182152dc4541f3c99a8dc7
Joey Wong <jjay9225@gmail.com>
2017-12-14 08:31:14 -0500

    Added expenses and filters reducer example

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,4 +1,71 @@
-import { createStore, combineReducers } from 'react';
+import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+
+//ADD_EXPENSE
+
+const addExpense = (
+    { description = '', 
+    note = '', 
+    amount = 0, 
+    createdAt = 0
+    }={}
+    ) => ({
+    type: 'ADD_EXPENSE',
+    expense:{
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
+
+//REMOVE_EXPENSE
+//EDIT_EXPENSE
+//SET_TEXT_FILTER
+//SORT_BY_DATE
+//SORT_BY_AMOUNT
+//SET_START_DATE
+//SET_END_DATE
+
+
+//expenses reducer
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state= expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+//Filters reducer
+
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+
+const filtersReducer = (state= filtersReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+};
+
+//store creation
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+
+console.log(store.getState());
 
 const demoState = {
     expense: [{
@@ -15,3 +82,4 @@ const demoState = {
         endDate: undefined
     }
 };
+
