d017521d2996557405da633e15d9f8773900c4ad
Joey Wong <joeyw@Joeys-MacBook-Pro.local>
2017-12-15 08:07:40 -0500

    Add and remove expense endgine added to redux-expensify.js example and introduced the spread operator

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,7 +1,31 @@
-import { createStore, combineReducers } from 'redux';
+import {createStore, combineReducers} from 'redux';
+import uuid from 'uuid';
 
 //ADD_EXPENSE
+
+const addExpense = ({
+                        description = '',
+                        note = '',
+                        amount = 0,
+                        createdAt = 0
+                    } = {}) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
 //REMOVE_EXPENSE
+
+const removeExpense = ({
+                           id
+                       }) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
 //EDIT_EXPENSE
 //SET_TEXT_FILTER
 //SORT_BY_DATE
@@ -14,8 +38,14 @@ import { createStore, combineReducers } from 'redux';
 
 const expensesReducerDefaultState = [];
 
-const expensesReducer = (state= expensesReducerDefaultState, action) => {
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [...state, action.expense]; //Same effect as below code, does not mutate state
+        //return state.concat(action.expense); //concat does not mutate array
+        case 'REMOVE_EXPENSE':
+            return state.filter(({id}) => id !== action.id); //destructured id from expense object
+
         default:
             return state;
     }
@@ -30,7 +60,7 @@ const filtersReducerDefaultState = {
     endDate: undefined
 };
 
-const filtersReducer = (state= filtersReducerDefaultState, action) => {
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
         default:
             return state
@@ -46,7 +76,14 @@ const store = createStore(
     })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100}));
+const expenseTwo = store.dispatch(addExpense({description: 'Coffee', amount: 200}));
+
+store.dispatch(removeExpense({id: expenseOne.expense.id}));
 
 const demoState = {
     expense: [{
