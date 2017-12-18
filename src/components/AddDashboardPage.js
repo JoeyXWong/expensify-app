ff101e647323c5f1e9f2621a7cf476ff49c0d549
Joey Wong <jjay9225@gmail.com>
2017-12-17 19:15:34 -0500

    Added expenseForm submit to add items to the list

--- src/components/AddDashboardPage.js
+++ src/components/AddDashboardPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { addExpense } from "../actions/expenses";
 
-const AddDashboardPage = () => (
+const AddDashboardPage = (props) => (
     <div>
         <h1>Add Expense</h1>
-        <ExpenseForm/>
+        <ExpenseForm
+            onSubmit={(expense) => {
+                props.dispatch(addExpense(expense));
+                props.history.push('/'); //browser routing
+            }}
+        />
     </div>
 );
 
-export default AddDashboardPage;
+export default connect()(AddDashboardPage);
